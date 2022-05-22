888f0459f20ac27338556f21675cc5852286de1c
irsc <iresearch.rad@gmail.com>
2022-05-22 16:52:57 +0700

    commit admin

--- routes/api.php
+++ routes/api.php
@@ -14,7 +14,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::group(['middleware' => 'cors'], function() {
+Route::post('/login', 'ApiController@login');
+
+Route::group(['middleware' => ['cors', 'auth:sanctum']], function() {
     Route::get('product', 'ApiController@productJson');
     Route::get('user/visit', 'ApiController@userVisit');
+
+    Route::post('/logout', 'ApiController@logout');
 });
